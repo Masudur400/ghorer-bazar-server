5c8efd23ead91a20ea2abaeb30fa8c2f6d265c73
Masudur400 <md8004210@gmail.com>
2024-08-16 20:57:07 +0600

    get cart by id

--- index.js
+++ index.js
@@ -129,12 +129,12 @@ async function run() {
     })
 
     // get carts items by id 
-    // app.get('/carts/id/:id', async (req, res) => {
-    //     const id = req.params.id
-    //     const query = { _id: new ObjectId(id) }
-    //     const result = await cartsCollection.findOne(query)
-    //     res.send(result)
-    // })
+    app.get('/carts/id/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await cartsCollection.findOne(query)
+        res.send(result)
+    })
 
     // delete cart by id 
     // app.delete('/carts/:id', async (req, res) => {
