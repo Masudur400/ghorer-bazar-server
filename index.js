cfcc870520b80d4103819c91a3c0c6d2c74650a7
Masudur400 <md8004210@gmail.com>
2024-08-16 20:58:05 +0600

    delete cart by id

--- index.js
+++ index.js
@@ -137,12 +137,12 @@ async function run() {
     })
 
     // delete cart by id 
-    // app.delete('/carts/:id', async (req, res) => {
-    //     const id = req.params.id
-    //     const query = { _id: new ObjectId(id) }
-    //     const result = await cartsCollection.deleteOne(query)
-    //     res.send(result)
-    // })
+    app.delete('/carts/:id', async (req, res) => {
+        const id = req.params.id
+        const query = { _id: new ObjectId(id) }
+        const result = await cartsCollection.deleteOne(query)
+        res.send(result)
+    })
 
 
 
