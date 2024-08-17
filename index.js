94f3aed9ff39f8ad1ed5dffeee7685fe13c9bed6
Masudur400 <md8004210@gmail.com>
2024-08-17 16:21:48 +0600

    user role update

--- index.js
+++ index.js
@@ -245,6 +245,14 @@ async function run() {
             res.send(result)
         })
 
+        // delete order by id 
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await ordersCollection.deleteOne(query)
+            res.send(result)
+        })
+
 
 
 
